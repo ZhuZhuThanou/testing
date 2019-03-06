2d50aae274398785e60097a0175ed0f504f8c7e2
thanou <thanou.mckinsey@gmail.com>
2019-03-06 00:18:47 -0500

    fixed sino stub on hard to test

--- codeSmell/hardToTestCode/src/UserDao.test.js
+++ codeSmell/hardToTestCode/src/UserDao.test.js
@@ -6,12 +6,22 @@ const { expect} = chai;
 const sinon = require('sinon');
 
 describe('UserDao', () => {
-  it('run getUser should return a value', ()=> {
-    let stub = sinon.stub();
-    Connection.getInstance().query = stub;
+  let stub;
+  const MOCK_QUERY_RESULT = 'sub query result';
+  beforeEach(() => {
+    //stub(obj, 'meth').callsFake(fn)
+    stub = sinon.stub(Connection.getInstance(), 'query').callsFake((queryString) => {
+      return MOCK_QUERY_RESULT;
+    });
+  });
+
+  afterEach(() => {
+    stub.restore();
+  });
+
+  it('invoke getUser', ()=> {    
     const userDao = new UserDao();
-    userDao.getUser('Id122');
-    console.log('after', stub.called);
-    expect(stub.called).to.be.true;
+    const value = userDao.getUser('Id122');
+    expect(value).to.equal(MOCK_QUERY_RESULT);
   });
 });
